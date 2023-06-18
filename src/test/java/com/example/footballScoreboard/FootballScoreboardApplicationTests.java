494e95030436dc100a751d4e305df59c6167ac44
noboni <noboni5199@gmail.com>
2023-06-18 18:50:14 +0200

    added test for start match

--- src/test/java/com/example/footballScoreboard/FootballScoreboardApplicationTests.java
+++ src/test/java/com/example/footballScoreboard/FootballScoreboardApplicationTests.java
@@ -1,10 +1,33 @@
 package com.example.footballScoreboard;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FootballScoreboardApplicationTests {
+
+	@Test
+	public void testStartMatch() {
+		// Create a mock scoreboard
+		ScoreBoard scoreboardMock = Mockito.mock(ScoreBoard.class);
+
+		// Define the expected match details
+		String homeTeam = "Mexico";
+		String awayTeam = "Canada";
+
+		List<Match> matches = scoreboardMock.getMatchesInProgress();
+		Match match = matches.get(0);
+
+		assertEquals(homeTeam, match.getHomeTeam());
+		assertEquals(awayTeam, match.getAwayTeam());
+		assertEquals(0, match.getHomeScore());
+		assertEquals(0, match.getAwayScore());
+	}
 
 	@Test
 	void contextLoads() {
