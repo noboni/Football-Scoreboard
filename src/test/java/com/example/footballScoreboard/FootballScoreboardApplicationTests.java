002d45b00c58f923cd73fb238de928a7cf130562
noboni <noboni5199@gmail.com>
2023-06-18 20:06:14 +0200

    finish match test case

--- src/test/java/com/example/footballScoreboard/FootballScoreboardApplicationTests.java
+++ src/test/java/com/example/footballScoreboard/FootballScoreboardApplicationTests.java
@@ -1,11 +1,6 @@
 package com.example.footballScoreboard;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -15,45 +10,60 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class FootballScoreboardApplicationTests {
 
-	@Test
-	public void testStartMatch() {
+    @Test
+    public void testStartMatch() {
 
-		ScoreBoard scoreBoard = new ScoreBoardImpl();
+        ScoreBoard scoreBoard = new ScoreBoardImpl();
 
-		// Define the expected match details
-		String homeTeam = "Mexico";
-		String awayTeam = "Canada";
+        // Define the expected match details
+        String homeTeam = "Mexico";
+        String awayTeam = "Canada";
 
-		scoreBoard.startMatch(homeTeam,awayTeam);
+        scoreBoard.startMatch(homeTeam, awayTeam);
 
-		List<Match> matches = scoreBoard.getMatchesInProgress();
-		Match match = matches.get(0);
+        List<Match> matches = scoreBoard.getMatchesInProgress();
+        Match match = matches.get(0);
 
-		assertEquals(homeTeam, match.getHomeTeam());
-		assertEquals(awayTeam, match.getAwayTeam());
-		assertEquals(0, match.getHomeScore());
-		assertEquals(0, match.getAwayScore());
-	}
+        assertEquals(homeTeam, match.getHomeTeam());
+        assertEquals(awayTeam, match.getAwayTeam());
+        assertEquals(0, match.getHomeScore());
+        assertEquals(0, match.getAwayScore());
+    }
 
-	@Test
-	public void testUpdateScore() {
+    @Test
+    public void testUpdateScore() {
 
-		ScoreBoard scoreBoard = new ScoreBoardImpl();
+        ScoreBoard scoreBoard = new ScoreBoardImpl();
 
-		// start a match
-		scoreBoard.startMatch("Mexico","Canada");
-		// Update the score
-		scoreBoard.updateScore("Mexico","Canada", 0, 5);
+        // start a match
+        scoreBoard.startMatch("Mexico", "Canada");
+        // Update the score
+        scoreBoard.updateScore("Mexico", "Canada", 0, 5);
 
-		// Assert that the score is updated
-		List<Match> matches = scoreBoard.getMatchesInProgress();
-		assertEquals(1, matches.size());
-		assertEquals(0, matches.get(0).getHomeScore());
-		assertEquals(5, matches.get(0).getAwayScore());
-	}
+        // Assert that the score is updated
+        List<Match> matches = scoreBoard.getMatchesInProgress();
+        assertEquals(1, matches.size());
+        assertEquals(0, matches.get(0).getHomeScore());
+        assertEquals(5, matches.get(0).getAwayScore());
+    }
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testFinishMatch() {
+        ScoreBoard scoreBoard = new ScoreBoardImpl();
+
+        // Start a new match
+        scoreBoard.startMatch("Mexico", "Canada");
+
+        // Finish the match
+        scoreBoard.finishMatch("Mexico", "Canada");
+
+        // Assert that the match is removed from the scoreboard
+        List<Match> matches = scoreBoard.getMatchesInProgress();
+        assertEquals(0, matches.size());
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
