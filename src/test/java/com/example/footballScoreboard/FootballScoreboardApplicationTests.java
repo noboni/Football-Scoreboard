b67f159ecf92fdaa0cc629ca9daf224c9a7cfdf8
noboni <noboni5199@gmail.com>
2023-06-18 19:50:27 +0200

    added test for update score

--- src/test/java/com/example/footballScoreboard/FootballScoreboardApplicationTests.java
+++ src/test/java/com/example/footballScoreboard/FootballScoreboardApplicationTests.java
@@ -1,6 +1,9 @@
 package com.example.footballScoreboard;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,11 +15,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class FootballScoreboardApplicationTests {
 
-	@Autowired
-	ScoreBoard scoreBoard;
-
 	@Test
 	public void testStartMatch() {
+
+		ScoreBoard scoreBoard = new ScoreBoardImpl();
 
 		// Define the expected match details
 		String homeTeam = "Mexico";
@@ -31,6 +33,23 @@ class FootballScoreboardApplicationTests {
 		assertEquals(awayTeam, match.getAwayTeam());
 		assertEquals(0, match.getHomeScore());
 		assertEquals(0, match.getAwayScore());
+	}
+
+	@Test
+	public void testUpdateScore() {
+
+		ScoreBoard scoreBoard = new ScoreBoardImpl();
+
+		// start a match
+		scoreBoard.startMatch("Mexico","Canada");
+		// Update the score
+		scoreBoard.updateScore("Mexico","Canada", 0, 5);
+
+		// Assert that the score is updated
+		List<Match> matches = scoreBoard.getMatchesInProgress();
+		assertEquals(1, matches.size());
+		assertEquals(0, matches.get(0).getHomeScore());
+		assertEquals(5, matches.get(0).getAwayScore());
 	}
 
 	@Test
