d7ab82c4fb9a7405a5d564a84bce1eed010e1359
noboni <noboni5199@gmail.com>
2023-06-18 17:38:38 +0200

    initial structure

---
+++ src/main/java/com/example/footballScoreboard/FootballScoreboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballScoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballScoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballScoreboardApplication.class, args);
+	}
+
+}
